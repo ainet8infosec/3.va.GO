dca8fb59d3d9d1c41ed8343ed3157e0bf85a3920
Alexandros A. Iosifidis <ainet8infosec@users.noreply.github.com>
2019-02-06 20:55:31 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-echo "Spinning up four VMs/droplets..."
+echo "Spinning up four 2GB RAM VMs/droplets..."
 
 for i in 1 2 3 4; do
   docker-machine create -d virtualbox \
@@ -9,39 +9,44 @@ for i in 1 2 3 4; do
     devnode-$i;
 done
 
+MANAGER=devnode-1
 
-echo "Initializing Swarm mode..."
+echo "Initializing Swarm mode, first node is the manager..."
 
-docker-machine ssh devnode-1 -- docker swarm init --advertise-addr $(docker-machine ip devnode-1)
+docker-machine ssh $MANAGER -- docker swarm init --advertise-addr $(docker-machine ip $MANAGER)
 
 
-echo "Adding the nodes to the Swarm..."
+echo "Adding rest nodes as workers to the Swarm..."
 
-TOKEN=`docker-machine ssh devnode-1 docker swarm join-token worker | grep token | awk '{ print $5 }'`
+TOKEN=`docker-machine ssh $MANAGER docker swarm join-token worker | grep token | awk '{ print $5 }'`
 
 for i in 2 3 4; do
   docker-machine ssh devnode-$i \
-    -- docker swarm join --token ${TOKEN} $(docker-machine ip devnode-1):2377;
+    -- docker swarm join --token ${TOKEN} $(docker-machine ip $MANAGER):2377;
 done
+
+echo "Preparing Swarm manager to handle helper services..."
+
+docker-machine ssh $MANAGER \
+  -- sudo mkdir -p /host/data
 
 echo "Preparing Swarm nodes to handle ELK stack..."
 
 for i in 1 2 3 4; do
   docker-machine ssh devnode-$i \
-  -- sudo sysctl -w vm.max_map_count=262144
+    -- sudo sysctl -w vm.max_map_count=262144
 done
-
 
 echo "Creating secret..."
 
-eval $(docker-machine env devnode-1)
-echo "3vaGO" | docker secret create secret_code -
+eval $(docker-machine env $MANAGER)
+echo "3.va.GO" | docker secret create secret_code -
 
 echo "Create a local DTR"
 
 docker service create --name registry --publish 50000:5000 registry:2
 
-echo "Build images and push them to local DTR"
+echo "Build Flask images and push them to local DTR"
 
 docker build -t localhost:50000/flask-docker-swarm_web:latest -f ./services/web/Dockerfile ./services/web
 docker push localhost:50000/flask-docker-swarm_web:latest
@@ -52,16 +57,16 @@ docker push localhost:50000/flask-docker-swarm_db:latest
 docker build -t localhost:50000/flask-docker-swarm_nginx:latest -f ./services/nginx/Dockerfile ./services/nginx
 docker push localhost:50000/flask-docker-swarm_nginx:latest
 
-echo "Deploying the Flask microservice..."
+echo "Deploying the Flask microservices..."
 
-docker stack deploy --compose-file=docker-compose-swarm.yml flask
+docker stack deploy --compose-file=docker-compose-swarm.yml flask_elk
 
 echo "Create the DB table and apply the seed..."
 
 sleep 60
-NODE=$(docker service ps -f "desired-state=running" --format "{{.Node}}" flask_web)
+NODE=$(docker service ps -f "desired-state=running" --format "{{.Node}}" flask_elk_web)
 eval $(docker-machine env $NODE)
-CONTAINER_ID=$(docker ps --filter name=flask_web --format "{{.ID}}")
+CONTAINER_ID=$(docker ps --filter name=flask_elk_web --format "{{.ID}}")
 docker container exec -it $CONTAINER_ID python manage.py recreate_db
 docker container exec -it $CONTAINER_ID python manage.py seed_db
 
@@ -69,5 +74,41 @@ docker container exec -it $CONTAINER_ID python manage.py seed_db
 echo "Get the IP address..."
 
 sleep 10
-eval $(docker-machine env devnode-1)
-docker-machine ip $(docker service ps -f "desired-state=running" --format "{{.Node}}" flask_nginx)
+eval $(docker-machine env $MANAGER)
+IP= $(docker-machine ip $(docker service ps -f "desired-state=running" --format "{{.Node}}" flask_elk_nginx))
+echo "NGINX is running on..."
+echo $IP
+
+echo "Build ELK images and push them to local DTR"
+
+docker build -t localhost:50000/logstash:5.3.2 -f ./services/elk/logstash/Dockerfile ./services/elk/logstash
+docker push localhost:50000/logstash:5.3.2
+
+echo "Populate the flask_elk stack with ELK services...."
+
+docker stack deploy --compose-file=docker-compose-elk-stack.yml flask_elk
+sleep 60
+
+echo "Bring up helper services...."
+
+eval $(docker-machine env $MANAGER)
+
+docker service create \
+    --name portainer \
+    --publish 9000:9000 \
+    --constraint 'node.role == manager' \
+    --mount type=bind,src=/host/data,dst=/data \
+    --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
+    portainer/portainer \
+    -H unix:///var/run/docker.sock
+
+docker build -t localhost:50000/jenkins-docker:latest -f ./services/jenkins/Dockerfile ./services/jenkins
+docker push localhost:50000/jenkins-docker:latest
+
+docker service create \
+    --name jenkinsCI \
+    --publish 8888:8888 \
+    --constraint 'node.role == manager' \
+    --mount type=bind,src=/host/data,dst=/data \
+    --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
+    localhost:50000/jenkins-docker:latest

7294bab785213023860c18bf905b22260f4c7e39
Alexandros A. Iosifidis <ainet8infosec@users.noreply.github.com>
2019-02-07 17:31:27 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -130,7 +130,7 @@ eval $(docker-machine env $MANAGER)
 CONTAINER_ID=$(docker ps --filter name=jenkinsCI --format "{{.ID}}")
 JENKINS_USER=$(docker container exec -it $CONTAINER_ID 'cat /run/secrets/jenkins-user')
 JENKINS_PASS=$(docker container exec -it $CONTAINER_ID 'cat /run/secrets/jenkins-pass')
-JENKINS_CRUMB=$(curl -s "http://$MANAGER_IP:8888/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)" -u "JENKINS_USER:JENKINS_PASS")
+JENKINS_CRUMB=$(curl -s "http://$MANAGER_IP:8888/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)" -u "$JENKINS_USER:$JENKINS_PASS")
 curl -s -XPOST "http://$MANAGER_IP:8888/createItem?name=testCI" \
     -u "$JENKINS_USER:$JENKINS_PASS" \
     --data-binary @testCI.xml \

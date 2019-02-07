d4dd906840c2992438651df058ce7ba841af98d4
Alexandros A. Iosifidis <ainet8infosec@users.noreply.github.com>
2019-02-07 17:35:12 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -128,8 +128,8 @@ echo "Build a testCI job for all the previous :D ...."
 sleep 10
 eval $(docker-machine env $MANAGER)
 CONTAINER_ID=$(docker ps --filter name=jenkinsCI --format "{{.ID}}")
-JENKINS_USER=$(docker container exec -it $CONTAINER_ID 'cat /run/secrets/jenkins-user')
-JENKINS_PASS=$(docker container exec -it $CONTAINER_ID 'cat /run/secrets/jenkins-pass')
+JENKINS_USER=$(docker container exec -it $CONTAINER_ID cat /run/secrets/jenkins-user)
+JENKINS_PASS=$(docker container exec -it $CONTAINER_ID cat /run/secrets/jenkins-pass)
 JENKINS_CRUMB=$(curl -s "http://$MANAGER_IP:8888/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)" -u "$JENKINS_USER:$JENKINS_PASS")
 curl -s -XPOST "http://$MANAGER_IP:8888/createItem?name=testCI" \
     -u "$JENKINS_USER:$JENKINS_PASS" \

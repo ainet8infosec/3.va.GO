41511fcd0ca63d1723d70bb69eaa29b667d16c90
Alexandros A. Iosifidis <ainet8infosec@users.noreply.github.com>
2019-02-07 22:56:59 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -129,10 +129,12 @@ sleep 10
 eval $(docker-machine env $MANAGER)
 CONTAINER_ID=$(docker ps --filter name=jenkinsCI --format "{{.ID}}")
 JENKINS_USER=$(docker container exec -it $CONTAINER_ID cat /run/secrets/jenkins-user)
+JENKINS_USER=$(echo $JENKINS_USER | tr -d '\r\n')
 JENKINS_PASS=$(docker container exec -it $CONTAINER_ID cat /run/secrets/jenkins-pass)
-#JENKINS_CRUMB=$(curl -s "http://$MANAGER_IP:8888/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)" -u "$JENKINS_USER:$JENKINS_PASS")
-curl -s -XPOST "http://$MANAGER_IP:8888/createItem?name=testCI" \
-    -u '"$JENKINS_USER":"$JENKINS_PASS"' \
+JENKINS_PASS=$(echo $JENKINS_PASS | tr -d '\r\n')
+#JENKINS_CRUMB=$(curl -s "http://${MANAGER_IP}:8888/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)" -u "$JENKINS_USER:$JENKINS_PASS")
+curl -s -XPOST "http://${MANAGER_IP}:8888/createItem?name=testCI" \
+    -u ${JENKINS_USER}:${JENKINS_PASS} \
     --data-binary @testCI.xml \
     -H "Content-Type:text/xml"
 

ff36338ac34d0d72717c5a82fea3fe8a40cb49aa
Alexandros A. Iosifidis <ainet8infosec@users.noreply.github.com>
2019-02-08 03:52:14 +0000

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -2,7 +2,15 @@
 
 num=`docker-machine ls --filter name=devnode --format "{{.Name}}" | wc -l`
 
-for ((i=1; i <= $num; i++)) ; do
-  docker-machine rm -y \
+if [ ! "$(docker service ls | grep devnode-1)" ]; 
+then
+  for ((i=$num+3; i <= $num+8; i++)) ; do
+    docker-machine rm -y \
     devnode-$i;
-done
+  done
+else
+  for ((i=1; i <= $num; i++)) ; do
+    docker-machine rm -y \
+    devnode-$i;
+  done
+fi
